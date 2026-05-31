d53527de70ab5f6ec54c8a33dafb8ad2326399c8
Lars Richter <github@n-develop.com>
2026-05-31 21:08:50 +0200

    Initial Commit

---
+++ AchtUndPunkt/AchtUndPunktApp.swift
@@ -0,0 +1,17 @@
+//
+//  AchtUndPunktApp.swift
+//  AchtUndPunkt
+//
+//  Created by Lars Richter on 31.05.26.
+//
+
+import SwiftUI
+
+@main
+struct AchtUndPunktApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
